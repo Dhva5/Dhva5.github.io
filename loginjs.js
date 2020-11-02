2b4c21af832b4b231072720975366b237be80675
Dhva5 <dhvanil_shah2007@atulvidyalaya.ac.in>
2020-11-02 14:05:45 +0530

    code

--- loginjs.js
+++ loginjs.js
@@ -1,3 +1,4 @@
+document.addEventListener('DOMContentLoaded') = function() {
 var modal = document.getElementById('id01');
 
 // When the user clicks anywhere outside of the modal, close it
@@ -5,4 +6,10 @@ window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
+}
+
+document.querySelector('.button').onclick = function() {
+    document.querySelector('#id01').style.display = 'none';
+    document.querySelector('.id011').style.display = 'block';
+}
 }
